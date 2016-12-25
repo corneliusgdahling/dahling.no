cc81c79fc95df8b48a4412e34be98c4f4bdebbf2
corneliusgdahling <corneliusgdahling@gmail.com>
2016-12-25 18:08:23 +0100

    added basic routing

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
+// import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute, Link, browserHistory } from 'react-router'
+import { render } from 'react-dom';
 import App from './App';
 import './index.css';
+import { IntlProvider, addLocaleData } from 'react-intl';
+import noLocaleData from 'react-intl/locale-data/no';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+
+addLocaleData(noLocaleData);
+
+  render(
+    (
+      // <App />), document.getElementById('root')
+      <IntlProvider locale="no">
+        <Router history={browserHistory}>
+          <Route path="/" component={App}>
+            <IndexRoute component={App} name="Home"/>
+            <Route path="hjem" component={App} name="HjemName" />
+          </Route>
+        </Router>
+      </IntlProvider>
+    ), document.getElementById('root')
+  );
