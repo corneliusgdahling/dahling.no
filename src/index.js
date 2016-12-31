d5c188354154ccb8055dfeaea148bf50a32b20a7
corneliusgdahling <corneliusgdahling@gmail.com>
2016-12-31 16:37:54 +0100

    Added route for home, projects and cv

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 import { render } from 'react-dom';
 import App from './App';
+import Home from './layouts/home/Home';
+import Projects from './layouts/projects/Projects';
+import CV from './layouts/cv/CV';
 import './index.css';
 import { IntlProvider, addLocaleData } from 'react-intl';
 import noLocaleData from 'react-intl/locale-data/no';
@@ -14,8 +17,10 @@ addLocaleData(noLocaleData);
       <IntlProvider locale="no">
         <Router history={browserHistory}>
           <Route path="/" component={App}>
-            <IndexRoute component={App} name="Home"/>
-            <Route path="hjem" component={App} name="HjemName" />
+            <IndexRoute component={App} name="Default"/>
+            <Route path="home" component={Home} name="Home" />
+            <Route path="projects" component={Projects} name="Projects" />
+            <Route path="cv" component={CV} name="cv" />
           </Route>
         </Router>
       </IntlProvider>
