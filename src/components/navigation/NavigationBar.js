e1c93e861d5fa4e2a460c05416914c40cb85a5d3
corneliusgdahling <corneliusgdahling@gmail.com>
2017-01-01 23:56:50 +0100

    Set up navigationbar using react-router-bootstrap

--- src/components/navigation/NavigationBar.js
+++ src/components/navigation/NavigationBar.js
@@ -1,33 +1,32 @@
 import React, { Component } from 'react';
 import { Navbar, Nav, NavItem } from 'react-bootstrap';
-import { Link } from 'react-router';
+import { LinkContainer } from 'react-router-bootstrap';
 import FontAwesome from 'react-fontawesome';
 
 export default class NavigationBar extends Component{
 
   render (){
     return (
-      <Navbar>
-        <Navbar.header>
-          <Navbar.brand>
-            <Link to="/">
-              <a className="nav-logo">Dahling.no</a>
-            </Link>
-          </Navbar.brand>
-        </Navbar.header>
-        <Navbar.collapse>
-          <Nav pullRight>
-                <Link to="/home">
-                  <NavItem eventKey={1}><FontAwesome name="home" /> Home</NavItem>
-                </Link>
-                <Link to="/projects">
-                  <NavItem eventKey={2}><FontAwesome name="projects" /> Projects</NavItem>
-                </Link>
-                <Link to="/cv">
-                  <NavItem eventKey={3}><FontAwesome name="cv" /> CV</NavItem>
-                </Link>
-              </Nav>
-        </Navbar.collapse>
+      <Navbar id="NavigationBar" collapseOnSelect >
+        <Navbar.Header>
+          <Navbar.Brand>
+            <LinkContainer to="/">
+            <a href="/" className="nav-logo"><FontAwesome name="home" /></a>
+            </LinkContainer>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Nav pullRight>
+          <LinkContainer to="/code">
+            <NavItem eventKey={1}><FontAwesome name="code" /> Code</NavItem>
+          </LinkContainer>
+          <LinkContainer to="/projects">
+            <NavItem eventKey={2}><FontAwesome name="github" /> Projects</NavItem>
+          </LinkContainer>
+          <LinkContainer to="/cv">
+            <NavItem eventKey={3}><FontAwesome name="file-pdf-o" /> CV</NavItem>
+          </LinkContainer>
+        </Nav>
       </Navbar>
     );
   }
