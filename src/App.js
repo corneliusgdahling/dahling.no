cc51c7aa3d76796349e579ff3ba9894551dd06bf
corneliusgdahling <corneliusgdahling@gmail.com>
2017-01-01 23:56:16 +0100

    Removed unecessary items and added support for loading components using routing

--- src/App.js
+++ src/App.js
@@ -3,14 +3,11 @@ import './App.css';
 import NavigationBar from './components/navigation/NavigationBar';
 
 export default class App extends Component {
-  name = "Cornelius";
   render() {
     return (
       <div className="App">
         <NavigationBar />
-        <h1>Velkommen til Dahling.no</h1>
-        <h2>{`Hei jeg heter ${name}`}</h2>
-        {console.log("test")}
+      {this.props.children}
       </div>
 
     );
