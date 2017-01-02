a4612f224ec4cbc286d2cc951f34afcb51d8b6dd
corneliusgdahling <corneliusgdahling@gmail.com>
2017-01-02 11:16:11 +0100

    Removed spelling mistake in h2 tag and made name a const instead of a global variable

--- src/layouts/home/Home.js
+++ src/layouts/home/Home.js
@@ -3,10 +3,10 @@ import React, { Component } from 'react';
 export default class Home extends Component{
 
   render(){
-    name = "Dahling.no";
+    const name = "Dahling.no";
     return(
       <div>
-        <h2>{`Velkomment til ${name}`}</h2>
+        <h2>{`Velkommen til ${name}`}</h2>
         {console.log("home")}
       </div>
     );
