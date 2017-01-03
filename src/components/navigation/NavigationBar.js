b42e2549fe82d8ea751b7da37b988baa0a61acae
corneliusgdahling <corneliusgdahling@gmail.com>
2017-01-03 13:45:17 +0100

    Added support for collapsing nav when on mobile, etc. Also changed NAvigationBar to className from id

--- src/components/navigation/NavigationBar.js
+++ src/components/navigation/NavigationBar.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Navbar, Nav, NavItem } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import FontAwesome from 'react-fontawesome';
+import './NavigationBar.css';
 
 export default class NavigationBar extends Component{
 
@@ -11,22 +12,24 @@ export default class NavigationBar extends Component{
         <Navbar.Header>
           <Navbar.Brand>
             <LinkContainer to="/">
-            <a href="/" className="nav-logo"><FontAwesome name="home" /></a>
+            <a href="/" className="nav-logo"><FontAwesome className="FontAwesome-home" name="home" /></a>
             </LinkContainer>
           </Navbar.Brand>
           <Navbar.Toggle />
         </Navbar.Header>
-        <Nav pullRight>
-          <LinkContainer to="/code">
-            <NavItem eventKey={1}><FontAwesome name="code" /> Code</NavItem>
-          </LinkContainer>
-          <LinkContainer to="/projects">
-            <NavItem eventKey={2}><FontAwesome name="github" /> Projects</NavItem>
-          </LinkContainer>
-          <LinkContainer to="/cv">
-            <NavItem eventKey={3}><FontAwesome name="file-pdf-o" /> CV</NavItem>
-          </LinkContainer>
-        </Nav>
+        <Navbar.Collapse>
+          <Nav pullRight>
+            <LinkContainer to="/code">
+              <NavItem className="code" eventKey={1}><FontAwesome name="code" /> Code</NavItem>
+            </LinkContainer>
+            <LinkContainer to="/projects">
+              <NavItem eventKey={2}><FontAwesome name="github" /> Projects</NavItem>
+            </LinkContainer>
+            <LinkContainer to="/cv">
+              <NavItem eventKey={3}><FontAwesome name="file-pdf-o" /> CV</NavItem>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
     );
   }
