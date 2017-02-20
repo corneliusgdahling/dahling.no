df79a0bcf20188546a593f1ef049964b63019cac
corneliusgdahling <corneliusgdahling@gmail.com>
2017-02-20 12:47:21 +0100

    Added 404 page

---
+++ src/layouts/not-found/NotFound.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './NotFound.css';
+
+export default class NotFound extends Component{
+
+  render(){
+    return(
+      <div>
+        <h2 id="not-found-title">The page you are trying to reach does not exist</h2>
+        {console.log("404 page")}
+      </div>
+    )
+  }
+}
