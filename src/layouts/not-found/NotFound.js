25cc2bb418c790781c431e58a204a8874766df9b
Cornelius Grieg Dahling <corneliusgdahling@gmail.com>
2019-03-13 15:11:54 +0100

    Test dev does not circleci

--- src/layouts/not-found/NotFound.js
+++ src/layouts/not-found/NotFound.js
@@ -6,7 +6,7 @@ export default class NotFound extends Component{
   render(){
     return(
       <div>
-        <h2 id="not-found-title">The page you are trying to reach does not exist</h2>
+        <h2 id="not-found-title">The page you are trying to reach does not exist. Why you do dis?</h2>
         {console.log("404 page")}
       </div>
     )
