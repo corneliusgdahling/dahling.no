dc0e0ece81026b2e93031401adaf3d68094d4dae
Cornelius Grieg Dahling <corneliusgdahling@gmail.com>
2019-03-13 13:35:25 +0100

    testing integration

--- src/layouts/home/Home.js
+++ src/layouts/home/Home.js
@@ -6,7 +6,7 @@ export default class Home extends Component{
     const name = "Dahling.no";
     return(
       <div>
-        <h1>{`Velkommen til ${name}`}</h1>
+        <h1>{`Hade til ${name}`}</h1>
         {console.log("home")}
       </div>
     );
